388fb4bb1bbb30b5457fb77261b24a0e4531a2a4
jose cahue <jose.cahue805@gmail.com>
2018-03-30 12:07:53 -0700

    Ball.h class example
    Added an example of the ball class. Using enum to define the direction the ball is traveling.

--- Ball.h
+++ Ball.h
@@ -1,22 +1,75 @@
 	#pragma once
 	#include "Shape.h"
-	class Ball :
-		public Shape
-	{
-	public:
-		Ball();
-		~Ball();
 
-		int speed() { return 0; }
-		void draw(int x, int y);
+enum Dir{STOP=0,LEFT=1,UPLEFT=2,DOWNLEFT=3,RIGHT=4,UPRIGHT=5,DOWNRIGHT=6}
+	
+class Ball :public Shape
+{
+    private:
+        int x,y;
+        int originalX, originalY;
+        Dir direction;
+    public:
+        Ball(int posX, int posY)
+        {
+            originalX=posX;
+            originalY=posY;
+            x=posX,y=posY;
+            direction=STOP;
+        }
+        ~Ball();
+    void Reset()
+    {
+        x=originalX;
+        y=originalY;
+        direction=STOP;
+        
+    }
+    void chageDirection(Dir d)
+    {
+        direction=d;
+    }
+    
+    int getX()const{return x;}
+    int getY()const {return y;}
+    Dir getDirection cost {return diredtion;}
+    
+    void Move()
+    {
+    switch (direction)
+    case STOP:
+        break;
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UPLEFT:
+        x--;y--;
+        break;
+    case DOWNLEFT:
+        x--;y++;
+        break;
+    case UPRIGHT:
+        x++;y--;
+        break;
+    case DOWNRIGHT:
+        x++;y++;
+        break;
+        default
+        break;
+        
+        
+    }
 
-		void collisionDetect(int xCo, int yCo) { } 
+        //int speed() { return 0; }
+        //void draw(int x, int y);
+        //void collisionDetect(int xCo, int yCo) { }
 
 
-	private:
-
-		int prevX, prevY;
+	
 
 
-	};
+};
 
