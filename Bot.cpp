dfd0bc26e7802126b0f7de78a4626b0699c3be6a
tarnaldo88 <tarnaldo88@gmail.com>
2018-04-03 19:45:02 -0700

    Update Bot.cpp

--- Bot.cpp
+++ Bot.cpp
@@ -7,6 +7,11 @@ extern void PlaceCursor(const int x, const int y);
 
 Bot::Bot()
 {
+	posY = 15;
+	posX = 119;
+
+	draw(posX, posY);
+
 }
 
 
@@ -16,6 +21,7 @@ Bot::~Bot()
 
 void Bot::draw(int x, int y)
 {
+	x = 119;
 	char paddle = 219;
 
 	PlaceCursor(x, y);
@@ -33,7 +39,13 @@ int Bot::speed()
 
 void Bot::difficulty(int a)
 {
-
+	//Default difficulty of 1
+	if (a == 1)
+	{
+		//first command to go to bottom of gameplay area
+		moveBot(0);
+		moveBot(26);
+	}
 }
 
 void Bot::tracking(int x, int y)
@@ -44,4 +56,23 @@ void Bot::tracking(int x, int y)
 void Bot::collisionDetect(int xCo, int yCo)
 {
 
+}
+
+void Bot::moveBot(int yn)
+{
+	int i = posY;
+
+	//function to move the paddle until it is in the new position
+	while (i != yn)
+	{
+		if (yn > posY)
+		{
+			draw(posX, ++i);
+		}
+		else if (yn < posY)
+		{
+			draw(posX, --i);
+		}
+	}
+	posY = y;
 }
